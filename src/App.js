fc6952e58db6827eea9052550d7f07a3c005a777
juliano batistim <jubatistim@gmail.com>
2022-05-30 17:23:45 -0500

    added number of players

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import web3 from './web3';
 import lottery from './lottery';
 import { useState, useEffect } from 'react';
 
 function App() {
 
   const [manager, setManager] = useState();
+  const [players, setPlayers] = useState();
 
   useEffect(() => {
     contractManager();
@@ -14,14 +13,18 @@ function App() {
 
   const contractManager = async () => {
     const _manager = await lottery.methods.manager().call();
-    console.log(_manager);
     setManager(_manager);
+
+    const _players = await lottery.methods.getPlayers().call();
+    console.log('AHHHHHHHHH');
+    console.log(_players.length.toString());
+    setPlayers(_players);
   };
 
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>The manager is {manager}</p>
+      <p>This contract is managed by {manager}. There are currently {players.length.toString()} people entered, competing to win 3.395 ether!</p>
     </div>
   );
 }
